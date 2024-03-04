31acd6c960651e83195036f1295a5a08f9ee9c25
DESKTOP-R1B1MF8\nh <yjkim2@wylie.co.kr>
2024-03-04 16:01:38 +0900

    블로그 목록 생성

---
+++ src/components/TheList.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item" v-for="(item, idx) in blogDoc" :key="idx">
+            <h2>{{ item.title }}</h2>
+            <p>{{ item.content }}</p>
+            <span>{{ item.date }}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'TheList',
+    props: {
+        blogDoc: Array,
+    }
+}
+</script>
